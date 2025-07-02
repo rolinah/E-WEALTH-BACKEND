122a22d6b7d6eb3298124350f76c0f57954087d7
Mark Mwazia Ghanzi <markmwazia@gmail.com>
2025-07-02 13:46:29 +0300

    i have added routes apart from admin and users

--- server/routes/community.js
+++ server/routes/community.js
@@ -1,1 +1,9 @@
-// Community routes 
+const express = require('express');
+const router = express.Router();
+const { getPostsByTopic, addPost } = require('../controllers/communityController');
+const auth = require('../middleware/authMiddleware');
+
+router.get('/:topicId', auth, getPostsByTopic);
+router.post('/', auth, addPost);
+
+module.exports = router;// Community routes 
--- server/routes/progress.js
+++ server/routes/progress.js
@@ -1,1 +1,8 @@
-// Progress routes 
+const express = require('express');
+const router = express.Router();
+const { updateProgress } = require('../controllers/progressController');
+const auth = require('../middleware/authMiddleware');
+
+router.post('/update', auth, updateProgress);
+
+module.exports = router;// Progress routes 
