891d643917c29d295b1c8c03d0da2d0586e82adb
rolinah <rolinahasiimwe@gmail.com>
2025-07-03 08:36:09 +0300

    database codes

--- server/config/db.js
+++ server/config/db.js
@@ -1,1 +1,15 @@
 // MySQL DB connection using Sequelize 
+const { Sequelize } = require('sequelize');
+require('dotenv').config();
+
+const sequelize = new Sequelize(
+  process.env.DB_NAME,
+  process.env.DB_USER,
+  process.env.DB_PASSWORD,
+  {
+    host: process.env.DB_HOST,
+    dialect: 'mysql',
+  }
+);
+
+module.exports = sequelize;
--- server/models/index.js
+++ server/models/index.js
@@ -1,1 +1,11 @@
 // Models index 
+const sequelize = require('../config/db');
+const User = require('./User');
+
+const db = {};
+db.Sequelize = sequelize;
+db.User = User;
+
+User.initModel(sequelize);
+
+module.exports = db;
